ddc04088a648d335a4bea86abfc4829f40a9235c
101German <gervika@list.ru>
2021-11-10 11:22:47 +0300

    update app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,26 +6,34 @@ import {MatPaginatorModule} from '@angular/material/paginator';
 import {MatSliderModule} from '@angular/material/slider'
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
 
 import { AppComponent } from './components/app/app.component';
-import { PoemsListTableComponent } from './components/poems-list-table/poems-list-table.component';
+import { PostsListTableComponent } from './components/posts-list-table/posts-list-table.component';
 import { TopBarComponent } from './components/top-bar/top-bar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { MatSortModule } from '@angular/material/sort';
+import { PostComponent } from './components/post/post.component';
+import { AuthorComponent } from './components/author/author.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    PoemsListTableComponent,
-    TopBarComponent
+    PostsListTableComponent,
+    TopBarComponent,
+    PostComponent,
+    AuthorComponent
   ],
   imports: [
     BrowserModule,
+    MatSortModule,
     MatTableModule,
     MatCardModule,
+    MatIconModule,
     MatSliderModule,
     MatPaginatorModule,
     MatProgressSpinnerModule,
