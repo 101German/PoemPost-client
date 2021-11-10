95a92d945e0639b2e61b86067cd8f761a62a9991
101German <gervika@list.ru>
2021-11-10 11:26:22 +0300

    Modify post service

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http'
+import { HttpClient, HttpParams } from '@angular/common/http'
 import { Post } from '../models/Post';
 import { textChangeRangeIsUnchanged } from 'typescript';
 import { Observable } from 'rxjs';
+import { PostParameters } from '../models/PostsParameters';
 
 
 @Injectable({
@@ -10,14 +11,21 @@ import { Observable } from 'rxjs';
 })
 export class PostService {
 
-private url = "https://localhost:5001/Post"
+  private url = "https://localhost:5001/Post"
 
   constructor(private http: HttpClient) { }
 
-  getPosts() {
-    return this.http.get<Post[]>(this.url);
+  getPosts(postParameters:PostParameters) : Observable<Post[]>
+   {
+    const params = new HttpParams()
+        .set('authorId', postParameters.authorId.toString());
+    return this.http.get<Post[]>(this.url,{params});
+  }
+
+  getPostById(id: number) {
+    return this.http.get<Post>(this.url + "/" + id)
 }
-  searchPosts(searchText:string){
-    return this.http.get<Post[]>(this.url + "?SearchTerm="+searchText)
+  searchPosts(searchText: string) {
+    return this.http.get<Post[]>(this.url + "?SearchTerm=" + searchText)
   }
 }
