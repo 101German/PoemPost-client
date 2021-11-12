2f42de4b50637c60708e464183ddf87d70d21834
101German <gervika@list.ru>
2021-11-12 19:04:29 +0300

    change postParameters

--- src/app/models/PostsParameters.ts
+++ src/app/models/PostsParameters.ts
@@ -1,5 +1,11 @@
+import { SortDirection } from "@angular/material/sort";
+
 export class PostParameters{
     constructor(
-    public authorId: number
+    public authorId: number = 0,
+    public pageNumber: number = 1,
+    public pageSize: number = 5,
+    public sortField: string = ' ',
+    public order: SortDirection = "asc"
     ){}
 }
