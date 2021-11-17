d2856ff27605a2c34093b8a51cf900435017e3dd
101German <gervika@list.ru>
2021-11-17 14:46:05 +0300

    Update author service

--- src/app/services/author.service.ts
+++ src/app/services/author.service.ts
@@ -1,7 +1,8 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Author } from '../models/Author';
+import { AuthorParameters } from '../models/AuthorParameters';
 
 @Injectable({
   providedIn: 'root'
@@ -13,5 +14,24 @@ export class AuthorService {
 
   public getAuthorById(id: number): Observable<Author> {
     return this.http.get<Author>(this.url+ "/" + id) 
+  }
+
+  public getAuthors(authorParameters:AuthorParameters) : Observable<HttpResponse<Author[]>>
+  {
+    let params = new HttpParams()
+        .set('pageNumber', authorParameters.pageNumber.toString())
+        .set('pageSize',authorParameters.pageSize.toString());
+         if(authorParameters.authorType !=0){
+           params = params.set('authorType',authorParameters.authorType.toString())
+         }
+        if(authorParameters.sortField!=' ')
+        {
+         params = params.append('OrderString',authorParameters.sortField + ' ' + authorParameters.order)
+        }
+
+        return this.http.get<Author[]>(this.url,{observe:'response',params:params})
+  }
+  public searchAuthors(searchString:string){
+    return this.http.get<Author[]>(this.url + "?SearchTerm=" + searchString)
   }
 }
