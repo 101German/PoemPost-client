da2774f1e2e1f191a827407df75f4ab1af9c1055
101German <gervika@list.ru>
2021-11-19 14:44:10 +0300

    Update app routing

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthorComponent } from './components/author/author.component';
+import { CategoriesGridListComponent } from './components/categories-grid-list/categories-grid-list.component';
+import { CategoryComponent } from './components/category/category.component';
 import { ClassicsListTableComponent } from './components/classics-list-table/classics-list-table.component';
 import { ContemporariesListTableComponent } from './components/contemporaries-list-table/contemporaries-list-table.component';
 import { PostComponent } from './components/post/post.component';
@@ -14,7 +16,9 @@ const routes: Routes = [];
     {path:'post/:id', component: PostComponent},
     {path:'author/:id',component: AuthorComponent},
     {path:'contemporaries',component: ContemporariesListTableComponent},
-    {path:'classics',component:ClassicsListTableComponent}])],
+    {path:'classics',component:ClassicsListTableComponent},
+    {path:'categories',component:CategoriesGridListComponent},
+    {path:'category/:id',component:CategoryComponent}])],
   exports: [RouterModule]
 })
 
