def9e70b13e895a40b9df40c067ebb40eb642d9a
101German <gervika@list.ru>
2021-11-26 16:47:55 +0300

    Modify post component

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { OAuthService } from 'angular-oauth2-oidc';
 import { Post } from 'src/app/models/Post';
 import { LikeService } from 'src/app/services/like.service';
 import { PostService } from 'src/app/services/post.service';
@@ -14,28 +15,32 @@ export class PostComponent implements OnInit {
   post!: Post
 
   constructor(
-    private route:ActivatedRoute,
-    private postService:PostService,
-    private likeService:LikeService) { }
+    private route: ActivatedRoute,
+    private postService: PostService,
+    private likeService: LikeService,
+    private oauthService: OAuthService) { }
 
   ngOnInit(): void {
     const routeParams = this.route.snapshot.paramMap;
     const postIdFromRoute = Number(routeParams.get('id'));
 
-    this.postService.getPostById(postIdFromRoute).subscribe((data:Post) => this.post = data);
+    this.postService.getPostById(postIdFromRoute).subscribe((data: Post) => this.post = data);
   }
 
-  like(userId:number,postId:number) {
-
-    this.likeService.addLike(userId,postId)
-    .subscribe((data:boolean) => {
-      if(!data){
-        this.post.likesCount-=1;
+  like(postId: number) {
+    if (this.oauthService.getAccessToken()) {
+      this.likeService.addLike(postId)
+        .subscribe((data: boolean) => {
+          if (!data) {
+            this.post.likesCount -= 1;
+          }
+          else {
+            this.post.likesCount += 1;
+          }
+        });
     }
-    else{
-      this.post.likesCount+=1;
+    else {
+      alert("Please authenticate in order to like")
     }
-    });
-    
   }
 }
