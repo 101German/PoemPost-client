7406779537e74cc7570b0524d44b955426429e5f
101German <gervika@list.ru>
2021-11-26 16:49:48 +0300

    Modify top-bar component

--- src/app/components/top-bar/top-bar.component.ts
+++ src/app/components/top-bar/top-bar.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { OAuthService } from 'angular-oauth2-oidc';
 
 @Component({
   selector: 'app-top-bar',
   templateUrl: './top-bar.component.html',
   styleUrls: ['./top-bar.component.css']
 })
-export class TopBarComponent implements OnInit {
+export class TopBarComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private oauthService: OAuthService) { }
+  public login(){
+    this.oauthService.initLoginFlow();
   }
-
+  
+  public isAuthenticated(){
+return this.oauthService.hasValidAccessToken();
+  }
+  public logOut(){
+    this.oauthService.logOut();
+  }
 }
